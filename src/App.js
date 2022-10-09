0d2a24a0e85bbdefddb56162ea7138d08da065ac
rachelallgaier <rayray.allgaier@gmail.com>
2022-10-08 23:56:06 -0400

    created navigation bar - currently not loading, unsure if its the code or computer issues

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './NavigationBar/NavBar'
+import './NavigationBar/styles.css'
+import projects from './Pages/Projects/projects'
+import experience from './Pages/Experience/experience'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    let Component
+    switch(window.location.pathname) {
+        case '/':
+            Component = App
+            break
+        case '/projects':
+            Component = projects
+            break
+        case '/experience':
+            Component = experience
+            break
+        default:
+            Component = App
+    }
+
+    return (
+        <>
+        <NavBar />
+        <Component />
+        </>
+    )
 }
 
 export default App;
---
+++ src/NavigationBar/NavBar.js
@@ -0,0 +1,13 @@
+ export default function NavBar () {
+     return <nav className="nav">
+         <a href="/" className="site-title"> Site Home </a>
+         <ul>
+             <li>
+                 <a href="/experience"> Experience</a>
+             </li>
+             <li>
+                 <a href="/projects"> Projects </a>
+             </li>
+         </ul>
+     </nav>
+ }
